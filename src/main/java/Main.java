1f0f6a61c8531dbf81c9375dab5d29ffae3ef10d
Edoardo Rossi <edd.rossi@gmail.com>
2015-01-30 09:36:19 +0100

    Einstein

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,66 +7,76 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.sql.*;
 
+import static javax.measure.unit.SI.KILOGRAM;
+import javax.measure.quantity.Mass;
+import org.jscience.physics.model.RelativisticModel;
+import org.jscience.physics.amount.Amount;
+
 public class Main extends HttpServlet {
-  @Override
-  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-      throws ServletException, IOException {
+	@Override
+		protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+		throws ServletException, IOException {
 
-    if (req.getRequestURI().endsWith("/db")) {
-      showDatabase(req,resp);
-    } else {
-      showHome(req,resp);
-    }
-  }
+			if (req.getRequestURI().endsWith("/db")) {
+				showDatabase(req,resp);
+			} else {
+				showHome(req,resp);
+			}
+		}
 
-  private void showHome(HttpServletRequest req, HttpServletResponse resp)
-      throws ServletException, IOException {
-    resp.getWriter().print("Hello from Java!");
-  }
+	private void showHome(HttpServletRequest req, HttpServletResponse resp)
+		throws ServletException, IOException {
+			resp.getWriter().print("Hello from Java... and Heroku!");
+			// Energy is compatible with mass (E=mc2)
+			RelativisticModel.select();
+			Amount<Mass> m = Amount.valueOf("12 GeV").to(KILOGRAM);
 
-  private void showDatabase(HttpServletRequest req, HttpServletResponse resp)
-      throws ServletException, IOException {
-    Connection connection = null;
-    try {
-      connection = getConnection();
+			resp.getWriter().print("E=mc^2: 12 GeV = " + m);
+		}
 
-      Statement stmt = connection.createStatement();
-      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+	private void showDatabase(HttpServletRequest req, HttpServletResponse resp)
+		throws ServletException, IOException {
+			Connection connection = null;
+			try {
+				connection = getConnection();
 
-      String out = "Hello!\n";
-      while (rs.next()) {
-          out += "Read from DB: " + rs.getTimestamp("tick") + "\n";
-      }
+				Statement stmt = connection.createStatement();
+				stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+				stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+				ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
 
-      resp.getWriter().print(out);
-    } catch (Exception e) {
-      resp.getWriter().print("There was an error: " + e.getMessage());
-    } finally {
-      if (connection != null) try{connection.close();} catch(SQLException e){}
-    }
-  }
+				String out = "Hello!\n";
+				while (rs.next()) {
+					out += "Read from DB: " + rs.getTimestamp("tick") + "\n";
+				}
 
-  private Connection getConnection() throws URISyntaxException, SQLException {
-    URI dbUri = new URI(System.getenv("DATABASE_URL"));
+				resp.getWriter().print(out);
+			} catch (Exception e) {
+				resp.getWriter().print("There was an error: " + e.getMessage());
+			} finally {
+				if (connection != null) try{connection.close();} catch(SQLException e){}
+			}
+		}
 
-    String username = dbUri.getUserInfo().split(":")[0];
-    String password = dbUri.getUserInfo().split(":")[1];
-    int port = dbUri.getPort();
+	private Connection getConnection() throws URISyntaxException, SQLException {
+		URI dbUri = new URI(System.getenv("DATABASE_URL"));
 
-    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ":" + port + dbUri.getPath();
+		String username = dbUri.getUserInfo().split(":")[0];
+		String password = dbUri.getUserInfo().split(":")[1];
+		int port = dbUri.getPort();
 
-    return DriverManager.getConnection(dbUrl, username, password);
-  }
+		String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ":" + port + dbUri.getPath();
 
-  public static void main(String[] args) throws Exception {
-    Server server = new Server(Integer.valueOf(System.getenv("PORT")));
-    ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-    context.setContextPath("/");
-    server.setHandler(context);
-    context.addServlet(new ServletHolder(new Main()),"/*");
-    server.start();
-    server.join();
-  }
+		return DriverManager.getConnection(dbUrl, username, password);
+	}
+
+	public static void main(String[] args) throws Exception {
+		Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+		context.setContextPath("/");
+		server.setHandler(context);
+		context.addServlet(new ServletHolder(new Main()),"/*");
+		server.start();
+		server.join();
+	}
 }
